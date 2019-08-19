4c455c5d951d3a524dc197bd9c415f827d427b6f
Sho Kusano <rosylilly@aduca.org>
2019-08-19 21:19:12 +0900

    Fix cache integration

--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -1,3 +1,3 @@
-Rails.application.configure do
+Rails.application.config.tap do |config|
   config.cache_store = :redis_cache_store, { driver: :hiredis, url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/' } }
 end
